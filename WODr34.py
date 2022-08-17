ded232279e8df4a1cb970a1f6b6fdefbd66a1bcd
Dirui <56666054+DiruiOwO@users.noreply.github.com>
2022-08-18 01:11:22 +0300

    Add files via upload

--- WODr34.py
+++ WODr34.py
@@ -1,15 +1,26 @@
-from lib2to3.pytree import convert
-from operator import concat
 import time
-from unittest import skip
 import webbrowser
-from random import randint, randrange
+from random import randint
+from colorama import Fore
+import os
+from bs4 import BeautifulSoup
+import requests
 
-print("Wheel of death (r34 edition)")
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+nonourl = "link"
+
+print(Fore.WHITE + "Wheel of death (r34 edition)")
 print("Not proudly made by dirui")
 print("") #seperator
 
-a = input("How many tabs?: ")
+input(Fore.GREEN + 'Press ENTER to begin')
+
+cls()
+
+a = input(Fore.WHITE + "How many tabs?: ")
 cona = int(a)
 print("") #seperator
 
@@ -17,17 +28,35 @@ t = input("How much time between tabs? (s): ")
 cont = int(t)
 print("") #seperator
 
+cls()
+
 print("0 - Custom (sometimes will bring you back to the main page)")
 print("1 - Websites random function")
 ver = input("What version do you want to use? 0/1: ")
 print("") #seperator
+
+cls()
 
 if ver == "0":
     for x in range(cona):
         num = randint(1000000, 9999999)
         connum = str(num)
         webbrowser.open_new_tab("https://rule34.xxx/index.php?page=post&s=view&id=" + connum)
-        print("ID = " + connum)
+
+        source = requests.get("https://rule34.xxx/index.php?page=post&s=view&id=" + connum).text
+        soup = BeautifulSoup(source, "lxml")
+        fsoup = soup.find("link", {"href":"https://rule34.xxx/index.php?page=post&s=list&tags=all"})
+
+        fsoupcon = str(fsoup)
+
+        print(Fore.WHITE + "ID = " + connum)
+
+        if nonourl in fsoupcon:
+            print(Fore.RED + "^ ID invalid ^")
+            #cona = cona + 1
+            #idk what to do when invalid, maybe someone could fork and fix it lol
+            #is it even posible to extend range() or for loop?????
+
         time.sleep(cont)
 
 if ver == "1":
@@ -40,7 +69,8 @@ if ver == "1":
         time.sleep(cont)
 
 print("") #seperator
-print("Done!")
+print(Fore.GREEN + "Done!")
 print("") #seperator
-input('Press ENTER to exit')
-    
+input(Fore.RED + 'Press ENTER to exit')
+
+cls()
